219d109f36e3c9c99cf0c699ad7bb5a3a9e81539
iWas-Coder <wasymatieh01@gmail.com>
2024-08-01 13:26:55 +0200

    Added basic editor entrypoint

---
+++ editor/editor.cc
@@ -0,0 +1,24 @@
+#include <volt.hh>
+
+#define XSTR(s) STR(s)
+#define STR(s) #s
+
+struct Editor : public volt::app::IAppBackend {
+  volt::app::AppSpec Init(void) final {
+    return {
+      .renderer {
+        .width     = 1280,
+        .height    = 720,
+        .name      = "Volt v" XSTR(VOLT_VERSION),
+        .max_fps   = 75,
+        .is_editor = true
+      }
+    };
+  }
+
+  void Start(volt::runtime::Scene &) final {
+    // ...
+  }
+};
+
+volt::app::IAppBackend *CreateVoltApp(void) { return new Editor; }
