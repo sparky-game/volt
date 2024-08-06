b172c500417dadd958e42ebf4539a64ee37a1e11
iWas-Coder <wasymatieh01@gmail.com>
2024-08-06 22:53:35 +0200

    fix: `consteval` -> `constexpr` for GCC

--- src/Core/NewtonMethod.hh
+++ src/Core/NewtonMethod.hh
@@ -2,7 +2,7 @@
 
 namespace volt::core {
   template <typename T = float>
-  inline consteval T NewtonMethod(T x, T curr, T prev) noexcept {
+  inline constexpr T NewtonMethod(T x, T curr, T prev) noexcept {
     return curr == prev ? curr : NewtonMethod<T>(x, 0.5f * (curr + x / curr), curr);
   }
 }
