1185d10ceedb3cb88963d5e3e72e754cb6216d79
iWas-Coder <wasymatieh01@gmail.com>
2024-08-16 22:16:52 +0200

    fix: forgot the namespace scope

--- src/Core/FileDialog_Apple.mm
+++ src/Core/FileDialog_Apple.mm
@@ -3,44 +3,46 @@
 #include <Cocoa/Cocoa.h>
 #include "FileDialog.hh"
 
-std::string FileDialog::Open(std::vector<std::string> &filter) {
-  @autoreleasepool {
-    NSOpenPanel *panel = [NSOpenPanel openPanel];
-    panel.canChooseFiles = YES;
-    panel.canChooseDirectories = NO;
-    panel.allowsMultipleSelection = NO;
-    if (not filter.empty()) {
-      NSMutableArray *file_types = [NSMutableArray array];
-      for (const auto &i : filter) {
-        [file_types addObject:[NSString stringWithUTF8String:filter.c_str()]];
+namespace volt::core {
+  std::string FileDialog::Open(std::vector<std::string> &filter) {
+    @autoreleasepool {
+      NSOpenPanel *panel = [NSOpenPanel openPanel];
+      panel.canChooseFiles = YES;
+      panel.canChooseDirectories = NO;
+      panel.allowsMultipleSelection = NO;
+      if (not filter.empty()) {
+        NSMutableArray *file_types = [NSMutableArray array];
+        for (const auto &i : filter) {
+          [file_types addObject:[NSString stringWithUTF8String:filter.c_str()]];
+        }
+        panel.allowedFileTypes = file_types;
       }
-      panel.allowedFileTypes = file_types;
+      if ([panel runModal] == NSModalResponseOK) {
+        NSURL *file_url = [[panel URLs] objectAtIndex:0];
+        return [[file_url path] UTF8String];
+      }
     }
-    if ([panel runModal] == NSModalResponseOK) {
-      NSURL *file_url = [[panel URLs] objectAtIndex:0];
-      return [[file_url path] UTF8String];
-    }
+    return {};
   }
-  return {};
-}
 
-std::string FileDialog::Save(std::vector<std::string> &filter) {
-  @autoreleasepool {
-    NSSavePanel *panel = [NSSavePanel savePanel];
-    panel.canCreateDirectories = YES;
-    if (not filter.empty()) {
-      NSMutableArray *file_types = [NSMutableArray array];
-      for (const auto &i : filter) {
-        [file_types addObject:[NSString stringWithUTF8String:filter.c_str()]];
+  std::string FileDialog::Save(std::vector<std::string> &filter) {
+    @autoreleasepool {
+      NSSavePanel *panel = [NSSavePanel savePanel];
+      panel.canCreateDirectories = YES;
+      if (not filter.empty()) {
+        NSMutableArray *file_types = [NSMutableArray array];
+        for (const auto &i : filter) {
+          [file_types addObject:[NSString stringWithUTF8String:filter.c_str()]];
+        }
+        panel.allowedFileTypes = file_types;
       }
-      panel.allowedFileTypes = file_types;
+      if ([panel runModal] == NSModalResponseOK) {
+        NSURL *file_url = [panel URL];
+        return [[file_url path] UTF8String];
+      }
     }
-    if ([panel runModal] == NSModalResponseOK) {
-      NSURL *file_url = [panel URL];
-      return [[file_url path] UTF8String];
-    }
+    return {};
   }
-  return {};
 }
 
 #endif  // defined(__APPLE__) && defined(__MACH__)
