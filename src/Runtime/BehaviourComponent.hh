49d6eaf3a481e022ddf1359546bbb1a427272ed4
iWas-Coder <wasymatieh01@gmail.com>
2024-09-16 22:12:34 +0200

    Created wrapper around `std::make_unique` to instantiate user scripts

--- src/Runtime/BehaviourComponent.hh
+++ src/Runtime/BehaviourComponent.hh
@@ -4,6 +4,14 @@
 #include "IBehaviour.hh"
 
 namespace volt::runtime {
+  template <typename T>
+  inline constexpr std::unique_ptr<T> Instantiate(void) {
+    auto ptr { std::make_unique<T>() };
+    // NOTE: `typeid(T).name()` is mangled
+    VOLT_LOG_INFO("Behaviour instantiated [{}|{}]", fmt::ptr(ptr.get()), typeid(T).name());
+    return ptr;
+  }
+
   struct BehaviourComponent {
     std::unique_ptr<IBehaviour> behaviour { nullptr };
   };
